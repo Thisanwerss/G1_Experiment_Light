f7694805b3b5470044022192f0e21f6a4ef1b60c
Victor Dhedin <victor.dhedin@tum.de>
2024-10-18 17:49:34 +0200

    Clean code and optimize.

--- mpc_controller/mpc.py
+++ mpc_controller/mpc.py
@@ -1,3 +1,4 @@
+import time
 from typing import Any, Dict, Tuple
 import numpy as np
 from bisect import bisect_right
@@ -19,13 +20,14 @@ class LocomotionMPC(ControllerAbstract):
                  pin_robot: PinQuadRobotWrapper,
                  gait_name: str = "trot",
                  sim_dt : float = 1.0e-3,
-                 **kwargs,
+                 print_info : bool = True,
+                 **kwargs
                  ) -> None:
         super().__init__(pin_robot)
-        self.debug = kwargs.get("debug", False)
         self.gait_name = gait_name
+        self.print_info = print_info
 
-        self.solver = QuadrupedAcadosSolver(pin_robot, gait_name)
+        self.solver = QuadrupedAcadosSolver(pin_robot, gait_name, print_info)
         config_opt = self.solver.config_opt
 
         self.Kp = self.solver.config_opt.Kp
@@ -37,7 +39,6 @@ class LocomotionMPC(ControllerAbstract):
         self.replanning_steps : int = int(1 / (self.replanning_freq * sim_dt))
         self.sim_step : int = 0
         self.current_opt_node : int = 0
-        self.initialized = False
 
         self.v_des : np.ndarray = np.zeros(3)
         self.w_yaw : float = 0.
@@ -77,14 +78,12 @@ class LocomotionMPC(ControllerAbstract):
 
         self.diverged : bool = False
 
-    def optimize(self, initialize: bool) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    def optimize(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
         return optimized trajectories.
         """
-        self.solver.init(self.current_opt_node, self.q_b_des, self.v_des, self.w_yaw, reset_traj=initialize)
-        if self.debug:
-            self.solver.print_contact_constraints()
-        q_sol, v_sol, a_sol, f_sol, dt_sol = self.solver.solve(print_stats=self.debug, print_time=self.debug)
+        self.solver.init(self.current_opt_node, self.q_b_des, self.v_des, self.w_yaw)
+        q_sol, v_sol, a_sol, f_sol, dt_sol = self.solver.solve()
 
         return q_sol, v_sol, a_sol, f_sol, dt_sol
 
@@ -102,20 +101,21 @@ class LocomotionMPC(ControllerAbstract):
 
         Returns:
             np.ndarray: trajectory interpolated at a 1/sim_freq
-        """        
+        """
         # Create an interpolation object that supports multi-dimensional input
         interp_func = interp1d(
             time_traj,
             traj,
             axis=0,
             kind=self.solver.config_opt.interpolation_mode,
-            fill_value="extrapolate"
+            fill_value="extrapolate",
+            assume_sorted=True,
             )
         
         # Interpolate the trajectory for all dimensions at once
         t_interpolated = np.linspace(0., time_traj[-1], int(time_traj[-1]/self.sim_dt)+1)
         interpolated_traj = interp_func(t_interpolated)
-        
+
         return interpolated_traj
 
     def get_traj(self,
@@ -135,14 +135,14 @@ class LocomotionMPC(ControllerAbstract):
         q_full_traj = [q0]
         ### todo: initial?
         v_full_traj = [np.zeros(self.robot.nv)]
-        initialize = True
+
         current_trajectory_time = 0.
         while current_trajectory_time < trajectory_time:
 
             self.robot.update(q_full_traj[-1], v_full_traj[-1])
             # Replan trajectory
-            q_traj, v_traj, _, _, dt_traj = self.optimize(initialize)
-            initialize = False
+            q_traj, v_traj, _, _, dt_traj = self.optimize()
+
             # Interpolate at sim_dt intervals
             time_traj = np.cumsum(dt_traj)
             q_traj_interp = self.interpolate_trajectory(q_traj, time_traj)
@@ -172,52 +172,53 @@ class LocomotionMPC(ControllerAbstract):
         Should be implemented by child classes.
         """
         if self._replan():
-            print("----------------------------------------------------------------")
-            print("replan!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            print("----------------------------------------------------------------")
-            # input()
-            # if self.initialized:
-            #     # fig, ax = plt.subplots(4, 3)
-            #     # for i in range(6):
-            #     #     ax.flatten()[i].plot(self.solver.q_sol_euler[:, i])
-            #     #     ax.flatten()[i + 6].plot(self.solver.v_sol_euler[:, i])
-            #     fig, ax = plt.subplots(4, 3)
-            #     for i in range(4):
-            #         for j in range(3):
-            #             ax[i, j].plot(self.f_plan[:, i, j])
-            #             # ax[i, j].plot(f_sol[:, i, j])
-            #     plt.show()
-            sim_time = robot_data.time
+            sim_time = round(robot_data.time, 4)
+
+            if self.print_info:
+                print("-"*40)
+                print("---- Replan", "node:", self.current_opt_node, f"at sim time {sim_time}")
+
             plan_step = 0
 
             # Find the optimization node of the last plan corresponding to the current simulation time
             self.current_opt_node += bisect_right(self.time_traj, sim_time)
-            print("Replan", "node:", self.current_opt_node, f"at sim time {sim_time}")
         
             # Update configuration from simulation
             self.robot.update(q, v)
             # Replan trajectory
-            if not self.initialized:
+            if self.sim_step == 0:
                 self.solver.set_max_iter(50)
-            else:
+                self.solver.set_nlp_tol(self.solver.config_opt.nlp_tol / 10.)
+                self.solver.set_qp_tol(self.solver.config_opt.qp_tol / 10.)
+            elif self.sim_step <= self.replanning_steps:
                 self.solver.set_max_iter(self.solver.config_opt.max_iter)
+                self.solver.set_nlp_tol(self.solver.config_opt.nlp_tol)
+                self.solver.set_qp_tol(self.solver.config_opt.qp_tol)
                 
-            q_sol, v_sol, a_sol, f_sol, dt_sol = self.optimize(initialize=(not self.initialized))
-            self.initialized = True
+            q_sol, v_sol, a_sol, f_sol, dt_sol = self.optimize()
+
             for i_foot, foot in enumerate(self.solver.dyn.feet):
                 f_sol[:, i_foot, :] = self.solver.params[foot.active.name][:, :-1].T * f_sol[:, i_foot, :]
-            # Interpolate plam at sim_dt intervals
+            
+            # Interpolate plan at sim_dt intervals
             traj_full = np.concatenate((
                 q_sol,
                 v_sol,
                 a_sol,
                 f_sol.reshape(-1, 12),
             ), axis=-1)
-            self.time_traj = np.cumsum(dt_sol)
-            self.q_plan = self.interpolate_trajectory(q_sol, self.time_traj)
-            self.v_plan = self.interpolate_trajectory(v_sol, self.time_traj)
-            self.a_plan = self.interpolate_trajectory(a_sol, self.time_traj - dt_sol[0])
-            self.f_plan = self.interpolate_trajectory(f_sol, self.time_traj - dt_sol[0])
+
+            self.time_traj = np.cumsum(dt_sol) - dt_sol[0]
+            # Take only 0.2s of the traj to save compute
+            traj_full_interp = self.interpolate_trajectory(traj_full, self.time_traj[: int(0.2 / self.sim_dt)])
+            self.q_plan, self.v_plan, self.a_plan, self.f_plan = np.split(
+                traj_full_interp,
+                [q_sol.shape[-1],
+                 q_sol.shape[-1] + v_sol.shape[-1], 
+                 q_sol.shape[-1] + v_sol.shape[-1] + a_sol.shape[-1]],
+                 axis=-1
+            )
+            self.f_plan = self.f_plan.reshape(-1, 4, 3)
 
             self.time_traj += sim_time
 
@@ -242,9 +243,5 @@ class LocomotionMPC(ControllerAbstract):
             in self.robot.joint_name2act_id.items()
         }
         self.sim_step += 1
-
-        # print(self.a_plan[plan_step, -12:])
-        # print(torques.flatten())
-        # print(torque_map)
 
         return torque_map
