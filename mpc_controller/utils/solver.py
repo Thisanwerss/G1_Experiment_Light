30393e675e825cb801770d3ff0dd45816f507605
Victor Dhedin <victor.dhedin@tum.de>
2025-02-27 16:19:56 +0000

    Update solver. Minor fix.

--- mpc_controller/utils/solver.py
+++ mpc_controller/utils/solver.py
@@ -85,9 +85,10 @@ class QuadrupedAcadosSolver(AcadosSolverHelper):
         self.update_cost_weights()
 
         # Init solution
-        self.q_sol_euler = np.zeros_like(self.states[self.dyn.q.name])
-        self.v_sol_euler = np.zeros_like(self.states[self.dyn.v.name])
-        self.a_sol = np.zeros_like(self.inputs[self.dyn.a.name])
+        self.q_sol_euler = np.zeros_like(self.states[self.dyn.q.name]).T
+        self.v_sol_euler = np.zeros_like(self.states[self.dyn.v.name]).T
+        self.a_sol = np.zeros_like(self.inputs[self.dyn.a.name]).T
+        self.h_sol = np.zeros_like(self.states[self.dyn.h.name]).T
         self.f_sol = np.zeros((self.config_opt.n_nodes, 4, 3))
         self.dt_node_sol = np.zeros((self.config_opt.n_nodes))
         
@@ -164,18 +165,16 @@ class QuadrupedAcadosSolver(AcadosSolverHelper):
         # Set the nominal time step
         if self.enable_time_opt:
             self.cost_ref["dt"][:] = self.dt_nodes
+
         self.cost_ref[self.dyn.base_cost.name][:] = base_ref[:, None]
+        self.cost_ref[self.dyn.swing_cost.name][:] = step_height
         self.cost_ref_terminal[self.dyn.base_cost.name] = base_ref_e
+        self.cost_ref_terminal[self.dyn.swing_cost.name][:] = step_height
 
         # Joint reference is nominal position with zero velocities
         joint_ref_vel = np.concatenate((joint_ref, np.zeros_like(joint_ref)))
         self.cost_ref[self.dyn.joint_cost.name] = joint_ref_vel[:, None]
         self.cost_ref_terminal[self.dyn.joint_cost.name] = joint_ref_vel.copy()
-        
-        if not self.restrict_cnt:
-            self.cost_ref[self.dyn.swing_cost.name][:] = self.height_offset + step_height
-            self.cost_ref_terminal[self.dyn.swing_cost.name][:] = self.height_offset + step_height
-
 
     def setup_initial_state(self,
                             q_euler : np.ndarray,
@@ -218,10 +217,11 @@ class QuadrupedAcadosSolver(AcadosSolverHelper):
             self.params[foot_cnt.plane_point.name][:] = np.zeros((3,1))
             self.params[foot_cnt.plane_point.name][-1, :] = self.height_offset
             self.params[foot_cnt.p_gain.name][:] = self.config_cost.W_foot_pos_constr_stab[i_foot]
-            self.params[foot_cnt.restrict.name][:] = 0.
             if self.restrict_cnt:
+                self.params[foot_cnt.restrict.name][:] = 0.
                 self.params[foot_cnt.range_radius.name][:] = self.config_cost.cnt_radius
             else:
+                self.params[foot_cnt.restrict.name][:] = 0.
                 self.params[foot_cnt.range_radius.name][:] = 1.0e10
 
     def setup_cnt_status(self,
@@ -252,7 +252,7 @@ class QuadrupedAcadosSolver(AcadosSolverHelper):
                 self.params[foot_cnt.restrict.name][0, :] = restrict
 
     @time_fn("setup_contact_plan")
-    def setup_contact_loc(self, contact_loc_plan : np.ndarray, step_height : float):
+    def setup_contact_loc(self, contact_loc_plan : np.ndarray):
         """
         Set the contact locations 
 
@@ -271,15 +271,9 @@ class QuadrupedAcadosSolver(AcadosSolverHelper):
         # Set reference for terminal contact
         for i_foot, foot_cnt in enumerate(self.dyn.feet):
             self.params[foot_cnt.plane_point.name] = contact_loc_plan[i_foot,].T
-            
-            # Reference cost distance to foothold
             self.cost_ref[foot_cnt.pos_cost.name] = contact_loc_plan[i_foot, 1:, :].T
             self.cost_ref_terminal[foot_cnt.pos_cost.name] = self.params[foot_cnt.plane_point.name][:, -1].T
-            
-            # Reference cost step height
-            self.cost_ref[self.dyn.swing_cost.name][i_foot, :] = contact_loc_plan[i_foot, 1:, -1] + step_height
-            self.cost_ref_terminal[self.dyn.swing_cost.name][i_foot, :] = contact_loc_plan[i_foot, -1, -1] + step_height
-
+        
     def print_contact_constraints(self):
         print()
         print("-"*10, "Contacts", "-"*10)
@@ -386,7 +380,7 @@ class QuadrupedAcadosSolver(AcadosSolverHelper):
 
         if self.restrict_cnt:
             assert not(cnt_locations is None), "Contact plan not provided"
-            self.setup_contact_loc(cnt_locations, step_height)
+            self.setup_contact_loc(cnt_locations)
 
         self.setup_initial_feet_pos(i_node)
 
@@ -408,32 +402,29 @@ class QuadrupedAcadosSolver(AcadosSolverHelper):
         super().solve(print_stats=self.print_info, print_time=self.print_info)
         self.parse_sol()
 
-        self.q_sol_euler = self.states[self.dyn.q.name].T.copy()
-
         # positions, [n_nodes + 1, 19]
-        # q_sol = ypr_to_quat_state_batched(self.q_sol_euler)
+        self.q_sol_euler[:] = self.states[self.dyn.q.name].T
         
         # velocities, [n_nodes + 1, 18]
-        self.v_sol_euler = self.states[self.dyn.v.name].T.copy()
-        # v_sol = v_glob_to_local_batched(self.q_sol_euler, self.v_sol_euler)
+        self.v_sol_euler[:] = self.states[self.dyn.v.name].T
 
         # centroidal momentum, [n_nodes + 1, 6]
-        self.h_sol = self.states[self.dyn.h.name].T.copy()
+        self.h_sol[:] = self.states[self.dyn.h.name].T
 
         # acceleration, [n_nodes, 18]
-        self.a_sol = self.inputs[self.dyn.a.name].T.copy()
+        self.a_sol[:] = self.inputs[self.dyn.a.name].T
 
         # end effector forces, [n_nodes, 4, 3]
-        self.f_sol = np.array([
+        self.f_sol[:] = np.array([
             self.inputs[f"f_{foot_cnt.frame_name}_{self.dyn.name}"]
             for foot_cnt in self.dyn.feet
         ]).transpose(2, 0, 1).copy()
 
         # dt time, [n_nodes, ]
         if self.enable_time_opt:
-            self.dt_node_sol = self.inputs["dt"].flatten().copy()
+            self.dt_node_sol[:] = self.inputs["dt"].flatten()
         else:
-            self.dt_node_sol = np.full((len(self.a_sol),), self.dt_nodes)
+            self.dt_node_sol[:] = np.full((len(self.a_sol),), self.dt_nodes)
 
         return self.q_sol_euler, self.v_sol_euler, self.a_sol, self.f_sol, self.dt_node_sol
 
